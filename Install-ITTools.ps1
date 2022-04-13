5185cc8eb6b65be6db6aa1a2d7b3cac50acac996
Tobias Schlienger <mail@tschlienger.ch>
2022-04-13 15:46:17 +0200

    Updated tools list and added functionality to install from winget or choco and select a specific version instead of latest

--- Install-ITTools.ps1
+++ Install-ITTools.ps1
@@ -1,9 +1,45 @@
-If (Not(Get-Command choco.exe -ErrorAction SilentlyContinue)) {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Try {
+    If (Get-Command winget) {
+        return $true
+    }
+}
+Catch {
+    write-host "WinGet is not installed. Please install it and try again." -ForegroundColor Red
+    start-process -FilePath "https://aka.ms/getwinget"
+    exit 1
 }
 
-$Packages = Import-Csv "$PSScriptRoot\tools.csv"
+Try {
+    If (Get-Command choco) {
+        return $true
+    }
+}
+Catch {
+    write-host "Chocolatey is not installed. Please install it and try again." -ForegroundColor Red
+    start-process -FilePath "https://chocolatey.org/install"
+    exit 1
+}
 
-Foreach ($Package in $Packages) {
-    choco install $Package.Item -y
+$ToolList = Import-Csv "$PSScriptRoot\tools.csv"
+
+Foreach ($Tool in $ToolList) {
+    Switch ($PackageManager) {
+        "winget" {
+            If ($Tool.Version -ne "") {
+                winget install $Tool.PackageName -v $Tool.Version -s winget
+            }
+            Else {
+                winget install $Tool.PackageName -s winget
+            }
+        }
+        "choco" {
+            If ($Tool.Version -ne "") {
+                choco install $Tool.PackageName --version=$Tool.Version -y
+            }
+            Else {
+                choco install $Tool.PackageName -y
+            }
+        }
+    }
+    winget install $Tool.PackageName -s winget
 }
