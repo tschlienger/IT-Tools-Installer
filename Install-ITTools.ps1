b90cac620f61fa9baa751a306b0d3c0bb7b52335
Tobias Schlienger <tobias.schlienger@gmail.com>
2023-12-05 14:58:49 +0100

    Uploaded newest version

--- Install-ITTools.ps1
+++ Install-ITTools.ps1
@@ -1,29 +1,16 @@
-Try {
-    If (Get-Command winget) {
-        return $true
-    }
-}
-Catch {
-    write-host "WinGet is not installed. Please install it and try again." -ForegroundColor Red
-    start-process -FilePath "https://aka.ms/getwinget"
+If (-Not(Get-Command winget)) {
     exit 1
 }
 
-Try {
-    If (Get-Command choco) {
-        return $true
-    }
-}
-Catch {
-    write-host "Chocolatey is not installed. Please install it and try again." -ForegroundColor Red
-    start-process -FilePath "https://chocolatey.org/install"
-    exit 1
-}
+#If (-Not(Get-Command choco)) {
+#    exit 1
+#}
 
 $ToolList = Import-Csv "$PSScriptRoot\tools.csv"
 
 Foreach ($Tool in $ToolList) {
-    Switch ($PackageManager) {
+    Read-Host "$($Tool.DisplayName)"
+    Switch ($Tool.PackageManager) {
         "winget" {
             If ($Tool.Version -ne "") {
                 winget install $Tool.PackageName -v $Tool.Version -s winget
